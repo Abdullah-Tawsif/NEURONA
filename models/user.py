2846250234648e929f1f6dccca9501b2d03233e0
S M Abdullah Tawsif <67152626+Abdullah-Tawsif@users.noreply.github.com>
2026-05-26 22:54:21 +0600

    Add files via upload

---
+++ models/user.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+
+from database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    username = Column(String, nullable=False)
+
+    email = Column(String, unique=True, nullable=False, index=True)
+
+    password = Column(String, nullable=False)
+
+    role = Column(String, nullable=False)  # admin / creator / investor
+
+    # 👇 NEW IMPORTANT FIELDS
+
+    is_verified = Column(Boolean, default=False)
+
+    is_active = Column(Boolean, default=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
