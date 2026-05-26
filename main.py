809d47e7746ed72d3c1b7d760db3070a522a2adb
S M Abdullah Tawsif <67152626+Abdullah-Tawsif@users.noreply.github.com>
2026-05-26 23:00:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from starlette.middleware.sessions import SessionMiddleware
+
+
 from routes.auth import router as auth_router
 from routes.index import router as index_router
 from routes.admin import router as admin_router
+from routes.creator import router as creator_router
+from routes import auth, admin, creator, index
 
+from database.database import engine
+from models.user import User
 
 app = FastAPI()
 
-# Static files
+# SESSION MIDDLEWARE
+app.add_middleware(
+    SessionMiddleware,
+    secret_key="neurona_secret_key"
+)
+
+# Static files 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Routers
 app.include_router(index_router)
 app.include_router(auth_router)
 app.include_router(admin_router)
+app.include_router(creator_router)
+
+#database
+User.metadata.create_all(bind=engine)
