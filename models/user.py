a9be5538346c148792dd8827e999255bc55adc86
S M Abdullah Tawsif <67152626+Abdullah-Tawsif@users.noreply.github.com>
2026-06-15 13:53:39 +0600

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,28 +1,18 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from datetime import datetime
+from datetime import datetime, timezone
 
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from database.database import Base
+
 
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-
     username = Column(String, nullable=False)
-
     email = Column(String, unique=True, nullable=False, index=True)
-
     password = Column(String, nullable=False)
-
-    role = Column(String, nullable=False)  # creator / investor
-
-    
-
-    # 👇 NEW IMPORTANT FIELDS
-
+    role = Column(String, nullable=False)
     is_verified = Column(Boolean, default=False)
-
     is_active = Column(Boolean, default=True)
-
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
