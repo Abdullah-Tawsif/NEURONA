7cefe2bd1725eae0620b364f5b9226a2c7912a9e
S M Abdullah Tawsif <67152626+Abdullah-Tawsif@users.noreply.github.com>
2026-06-15 14:02:45 +0600

    Update login.js

--- static/js/login.js
+++ static/js/login.js
@@ -1,20 +1,10 @@
-// Auto-hide flash messages
-  window.setTimeout(function() {
-
-    const flashes = document.querySelectorAll('.flash-message');
-
-    flashes.forEach(flash => {
-
-      flash.style.transition = 'opacity 0.5s ease-out';
-      flash.style.opacity = '0';
-
-      setTimeout(() => {
-        flash.remove();
-      }, 500);
-
-    });
-
-  }, 4000);
-
-
-src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+window.setTimeout(function () {
+  const flashes = document.querySelectorAll('.flash-message');
+  flashes.forEach(function (flash) {
+    flash.style.transition = 'opacity 0.5s ease-out';
+    flash.style.opacity = '0';
+    setTimeout(function () {
+      flash.remove();
+    }, 500);
+  });
+}, 4000);
